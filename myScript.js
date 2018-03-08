a3da1d19d4dd35c3087aba1652c2521d420561e1
Ninetails <jose_rafael93@hotmail.com>
2018-03-08 02:17:08 -0500

    Update
    JS still broken, added back to top and fixed go to anchor

---
+++ myScript.js
@@ -0,0 +1,39 @@
+// SMOOTH SCROLL
+
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+// STICKY BACK TO TOP
+
+$(document).ready(function(){
+  
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn();
+    } else {
+      $('.back-to-top').fadeOut();
+    }
+  });
+  
+  //Click event to scroll to top
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+  
+});
