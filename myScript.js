0ebed63e7088ca5392acd3f257c2168dc10582b0
Ninetails <jose_rafael93@hotmail.com>
2018-03-21 22:13:30 -0400

    Update
    Content Update.
    Minor Fixes.

--- myScript.js
+++ myScript.js
@@ -1,39 +1,42 @@
-// SMOOTH SCROLL
+// SMOOTH SCROLL -----------------------------------------------------------------------------
 
-function scrollNav() {
-  $('.nav a').click(function(){  
-    //Toggle Class
-    $(".active").removeClass("active");      
-    $(this).closest('li').addClass("active");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active');
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 160
-    }, 400);
-    return false;
-  });
-  $('.scrollTop a').scrollTop();
-}
-scrollNav();
+$("#nav ul li a[href^='#']").on('click', function(e) {
 
-// STICKY BACK TO TOP
+   // prevent default anchor click behavior
+   e.preventDefault();
 
-$(document).ready(function(){
-  
-  //Check to see if the window is top if not then display button
-  $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn();
-    } else {
-      $('.back-to-top').fadeOut();
-    }
-  });
-  
-  //Click event to scroll to top
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},800);
-    return false;
-  });
-  
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
 });
+
+// STICKY BACK TO TOP ------------------------------------------------------------------------
+
+// $(document).ready(function(){
+  
+//   //Check to see if the window is top if not then display button
+//   $(window).scroll(function(){
+//     if ($(this).scrollTop() > 100) {
+//       $('.back-to-top').fadeIn();
+//     } else {
+//       $('.back-to-top').fadeOut();
+//     }
+//   });
+  
+//   //Click event to scroll to top
+//   $('.back-to-top').click(function(){
+//     $('html, body').animate({scrollTop : 0},800);
+//     return false;
+//   });
+  
+// });
